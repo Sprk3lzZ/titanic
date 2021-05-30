dfac153e60fb163bb70c0a3619ac2ff4f3035d41
Yanis Zeghiche <yanis.zge@icloud.com>
2021-05-30 20:27:07 +0200

    final

--- Titanic.py
+++ Titanic.py
@@ -1,26 +1,37 @@
 import pandas
-from sklearn.model_selection import *
-from sklearn.linear_model import *
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score
-from sklearn.linear_model import LogisticRegression
 
-model = LogisticRegression()
+def starter():
+    """
+    Fonction qui initialise le tableeau via le csv.
+    :return: lists
+    """
 
-tab = pandas.read_csv("titanic.csv", sep=',')
+    tab = pandas.read_csv("titanic.csv", sep=',')
 
-newtab = tab.loc[:, ['Survived', 'Pclass', 'Sex', 'Age', 'Embarked']]
-newtab.Age = newtab.Age.fillna(0)
+    newtab = tab.loc[:, ['Survived', 'Pclass', 'Sex', 'Age', 'Embarked']]
+
+    return tab, newtab
+
 
 def modif():
     """
-    :return:
+    Fonctin qui modifie et selectionne les informations les plus utiles pour la recherche.
+    :return: list
     """
 
-    tabmodif = tab.loc[:, ['Survived', 'Pclass', 'Sex', 'Age', 'Embarked']]
-    tabmodif.Age = tabmodif.Age.fillna(0)
+    tab, newtab = starter()
+    tabmodif = tab.loc[:, ['Survived', 'Pclass', 'Sex', 'Age', 'Embarked']] #Selection des catégories qui nous intéresse.
 
-    for i in range(len(tabmodif)):
+    tabmodif.Age = tabmodif.Age.fillna(0) #Transforme les ages inconnus à 0.
+
+    for i in range(len(tabmodif)):    #Boucle servant à attribuer aux valeurs du csv des valeurs utilisables/pratiques.
+        if (tabmodif.Age[i] == 0):
+            nb = 0
+            for e in range(i):
+                nb += tabmodif.Age[e]
+            nb = nb / (i + 1)
+            newtab.Age[i] = int(nb)
         if tabmodif.Sex[i] == 'male':
             tabmodif.Sex[i] = 0
         elif tabmodif.Sex[i] == 'female':
@@ -32,28 +43,45 @@ def modif():
         elif tabmodif.Embarked[i] == 'Q':
             tabmodif.Embarked[i] = 2
         else:
-            tabmodif.Embarked[i] = 3
+            tabmodif.Embarked[i] = tabmodif.Embarked[i - 1]
     return tabmodif
 
-def main(k,longueur, largeur,a,b):
+
+def main(k, classe,sexe,age,embarquement):
     """
-    :return:
+    Fonction qui teste si la personne qui entre ses informations serait en vie après le drame du titanic
+    :return: sting
     """
     tabt = modif()
-    x = tabt.loc[:, "Pclass"]
-    y = tabt.loc[:, "Sex"]
-    z = tabt.loc[:, "Age"]
-    w = tabt.loc[:, "Embarked"]
-    tab2 = list(zip(x,y,z,w))
-    print(tab2)
+    t_classe = tabt.loc[:, "Pclass"]                         #Créations des tableaux par catégorie.
+    t_sexe = tabt.loc[:, "Sex"]
+    t_age = tabt.loc[:, "Age"]
+    t_embarquement = tabt.loc[:, "Embarked"]
+    tab2 = list(zip(t_classe, t_sexe, t_age, t_embarquement)) #Création du tableau de tuples.
     lab = tabt.loc[:, "Survived"]
     model = KNeighborsClassifier(n_neighbors=k)
-    print("model v1: ", model)
     model.fit(tab2, lab)
-    print("model v2: ", model)
-    prediction = model.predict([[longueur, largeur,a,b]])
-    print("prediction : ", prediction)
+    prediction = model.predict([[classe,sexe,age,embarquement]])
+
+    if prediction == 0:
+        print("Desolé, mais vous n'auriez pas survecu...")
+    else:
+        print("Super ! Vous avez l'étoffe de la personne qui aurait survécu !")
+
+
+def questions():
+    """
+    Fonction qui demande les informations à l'utilisateur.
+    :return:
+    """
+
+    classe = float(input("Veuilliez rentrer votre classe : "))
+    sexe = float(input("Veuilliez rentrer votre sexe ( 1 : femme / 0 : homme ) : "))
+    age = float(input("Veuilliez rentrer votre age : "))
+    embarquement = float(input("Veuilliez rentrer votre numero d'embarquement : "))
+
+    main(5,classe,sexe,age,embarquement)
 
 
 
-main(5,1,0,80,1)
+questions()
